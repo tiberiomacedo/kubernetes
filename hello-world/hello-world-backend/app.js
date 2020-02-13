9502f4aa9dcebf388954cc558d23ebb34c80dfac
Tibério <tiberiomacedo@gmail.com>
2020-02-13 17:34:32 -0300

    update

---
+++ hello-world/hello-world-backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+
+router.get('/hwbff', (req, res) => {
+    console.log(req.headers);
+    res.json({ message: 'Hello World' });
+});
+
+app.use('/', router);
+
+app.listen(3040, function () {
+    console.log('App listening on port 3040!');
+});
