070dcbc1564fa0571b0ab70366f113d78e44242b
Tibério <tiberiomacedo@gmail.com>
2020-03-27 11:08:29 -0300

    update

--- hello-world/hello-world-backend/app.js
+++ hello-world/hello-world-backend/app.js
@@ -2,14 +2,20 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 
-router.get('/v1/hello', (req, res) => {
+const contextPath = '/mvbff/greetings';
+const { version } = require('./package.json');
+
+router.get(contextPath + '/v1/hello', (req, res) => {
     console.log('timestamp: ' + new Date().getTime());
     res.json({ message: 'Hello World!' });
 });
 
+router.get('/version', (req, res) => {
+  res.status(200).json({ version });
+});
+
 router.get('/healthcheck', (req, res) => {
-    console.log('timestamp: ' + new Date().getTime());
-    res.json({ status: 'UP' });
+  return res.status(200).json({ status: 'UP' });
 });
 
 app.use('/', router);
